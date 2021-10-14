77d73cd026198ab00a841f98e7732a35a535249a
Tejomay Saha <tejsaha@WKMZTB6470BB.global.publicisgroupe.net>
2021-10-14 16:57:18 +0530

    lazy load added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router";
 import Layout from "./components/layout/Layout";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
-import QuoteDetails from "./pages/QuoteDetail";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const QuoteDetails = React.lazy(() => import("./pages/QuoteDetail"));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/new-quote">
-          <NewQuote />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetails />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetails />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
